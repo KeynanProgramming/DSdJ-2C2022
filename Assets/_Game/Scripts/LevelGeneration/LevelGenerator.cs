a4c8c87a0178afa186338b7136600168c27bea59
Durzdor <maxiperez024@hotmail.com>
2022-09-17 16:04:45 -0300

    Squashed commit of the following:
    commit 45d5d5e01514320bceb75a5866abedd4dab9b63e
    Merge: b0d46a7 b82fa98
    Author: Raxipaxi <razielferundir@gmail.com>
    Date:   Fri Sep 16 18:24:37 2022 -0300
    
        Merge branch 'MaxiBranch' into Develop
    
    commit b0d46a783af098d410146c8d33805a60060566d5
    Merge: 59c07e4 92ca963
    Author: Raxipaxi <razielferundir@gmail.com>
    Date:   Fri Sep 16 18:24:12 2022 -0300
    
        Merge branch 'Level-Generator' into Develop
    
    commit 92ca963d993413cf1c3c5f7409e3bd355a462dac
    Author: Huergacho <75048402+Huergacho@users.noreply.github.com>
    Date:   Fri Sep 16 14:19:02 2022 -0300
    
        Obstacles Not working
    
    commit 06a8ce0c7585ff6e06f4c03e99af5195b0aa3156
    Author: Huergacho <75048402+Huergacho@users.noreply.github.com>
    Date:   Fri Sep 16 11:34:58 2022 -0300
    
        Level Generation
    
    commit 59c07e4947b4d9e5f9311899643865bede35d5d1
    Author: Durzdor <maxiperez024@hotmail.com>
    Date:   Fri Sep 16 04:01:05 2022 -0300
    
        Squashed commit of the following:
    
        commit b82fa98b22562b72a82488f48e9509f6fd4aac6e
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Fri Sep 16 02:50:55 2022 -0300
    
            ADD Interactables
    
            + Base for interactions pressing a button
    
        commit e774d748ae3beeca1466cf05b5fbab4aa513325e
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Fri Sep 16 00:19:59 2022 -0300
    
            ADD Knockback Logic
    
            + Enemy goes opposite to where the projectile collides
            * Enemy intereactions with rigidbodys
    
        commit 8cc7b494b18757ad4ff928b3b9fdc8957ebf368b
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Thu Sep 15 22:49:18 2022 -0300
    
            ADD Pierce Logic
    
            + Goes through Pierce number of enemies before destroying itself
    
        commit cf7f5b0d1fd7b89a25e3bce663dbf77421eb50d1
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Thu Sep 15 22:00:09 2022 -0300
    
            ADD Projectile Interactions
    
            + Multiple simultaneous projectiles
            + Attack speed
            + Damage to enemy (health)
    
        commit e5d60ab95cd3b44690cc6d41776adb1580555609
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Thu Sep 15 04:13:11 2022 -0300
    
            ADD Player Combat
    
            + Player Top down Movement
            + Player Mouse tracking
            + Player Shooting to mouse position
    
        commit 902b139a9e31a0d55245c0d3e619c36b9c3ea61b
        Author: Durzdor <maxiperez024@hotmail.com>
        Date:   Wed Sep 14 17:28:56 2022 -0300
    
            Squashed commit of the following:
    
            commit 4314ed40dfbd39acf2ae57ee96a53e107e9f37a5
            Author: Raxipaxi <razielferundir@gmail.com>
            Date:   Wed Sep 14 14:48:02 2022 -0300
    
                Nuevo fix del pack dungeon
    
            commit 1e9011b77cb31fefd0e1e60df3479aa2ea59c538
            Author: Raxipaxi <razielferundir@gmail.com>
            Date:   Wed Sep 14 14:23:16 2022 -0300
    
                Fix de FX
    
    commit 4314ed40dfbd39acf2ae57ee96a53e107e9f37a5
    Author: Raxipaxi <razielferundir@gmail.com>
    Date:   Wed Sep 14 14:48:02 2022 -0300
    
        Nuevo fix del pack dungeon
    
    commit 1e9011b77cb31fefd0e1e60df3479aa2ea59c538
    Author: Raxipaxi <razielferundir@gmail.com>
    Date:   Wed Sep 14 14:23:16 2022 -0300
    
        Fix de FX

---
+++ Assets/_Game/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Node[] nodes;
+    private Transform playerSpawn;
+
+    private void Start()
+    {
+        AsignateBossRoom();
+        InstaceRooms();
+    }
+
+    void InstaceRooms()
+    {
+        foreach (var item in nodes)
+        {
+            item.InstanceRoom();
+        }
+    }
+    void AsignateBossRoom()
+    {
+        var index = Random.Range(0, nodes.Length);
+        nodes[index].isBossRoom = true;
+    }
+
+}
