3778f994a99ff628fc0691743be9c22fd879ed67
Raxipaxi <razielferundir@gmail.com>
2022-09-26 15:02:02 -0300

    Scripts de Line Of Sight y Obstacle avoidance
    Para no depender de un nav mesh

---
+++ Assets/_Game/Scripts/Entities/Enemies/EnemyModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+ class EnemyModel : MonoBehaviour
+ {
+     public EnemySO data;
+     public Rigidbody rb;
+     protected Transform _transform;
+     
+     private void Awake()
+     {
+         rb = GetComponent<Rigidbody>();
+         _transform = transform;
+     }
+
+     public virtual void Idle()
+     {
+         rb.velocity = Vector3.zero;
+     }
+     
+     public virtual void Move(Vector3 dir)
+     {
+         rb.velocity = dir.normalized * data.speed;
+     }
+
+     public virtual void LookAt(Vector3 dir)
+     {
+         _transform.forward = dir.normalized;
+     }
+
+     public virtual void Attack()
+     {
+         
+     }
+
+     public virtual void Die()
+     {
+         
+     }
+     
+     
+
+
+ }
