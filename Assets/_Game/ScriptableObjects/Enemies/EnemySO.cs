3f18c1ccf3ba08993e16f202b74c2617424007fa
Raxipaxi <razielferundir@gmail.com>
2022-09-26 12:44:36 -0300

    AI Test

---
+++ Assets/_Game/ScriptableObjects/Enemies/EnemySO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemy", menuName = "Enemy", order = 0)]
+public class EnemySO : ScriptableObject
+{
+    public float maxLife;
+    public float speed;
+    public float damage;
+}
---
+++ Assets/_Game/Scripts/Entities/EnemyModel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+ class EnemyModel : MonoBehaviour
+ {
+     public EnemySO data;
+
+     
+     public virtual void Idle()
+     {
+         
+     }
+     
+     public virtual void Move(Vector3 dir)
+     {
+         
+     }
+
+     public virtual void Attack()
+     {
+         
+     }
+
+     public virtual void Die()
+     {
+         
+     }
+     
+     
+
+
+ }
