75586b35cd382ac948b4f731021e3bf3b56f90fc
Durzdor <maxiperez024@hotmail.com>
2022-09-26 04:42:49 -0300

    CHANGE Projectile stat setup
    + Can setup movement speed on instantiate
    + Can setup lifespan on instantiate
    Both have default values so they are not required to instantiate

--- Assets/_Game/Scripts/Generic/Projectile.cs
+++ Assets/_Game/Scripts/Generic/Projectile.cs
@@ -2,30 +2,32 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float lifespan = 3f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float lifespan = 2f;
     [SerializeField] private Collider sphereSolid;
-    
+
     public int Damage { get; private set; }
     public int Pierce { get; private set; }
     public float KnockBack { get; private set; }
 
-    public void StatSetup(int damage, int pierce, float knockBack)
+    public void StatSetup(int damage, int pierce, float knockBack, float speed = 0, float life = 0)
     {
         Damage = damage;
         Pierce = pierce;
         KnockBack = knockBack;
+        if (speed != 0) moveSpeed = speed;
+        if (life != 0) lifespan = life;
     }
-    
+
     private void Start()
     {
-        Destroy(gameObject,lifespan);
+        Destroy(gameObject, lifespan);
     }
 
     private void Update()
     {
         var t = transform;
-        t.position += t.forward * (speed * Time.deltaTime);
+        t.position += t.forward * (moveSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,36 +37,29 @@ public class Projectile : MonoBehaviour
             var go = other.gameObject.GetComponent<Health>();
             go.TakeDamage(Damage);
             Pierce--;
-            if (Pierce <= 0)
-            {
-                Destroy(gameObject);
-            }
+            if (Pierce <= 0) Destroy(gameObject);
         }
     }
-    
+
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Enemy")) return;
         var contact = collision.contacts[0];
         var contactPos = contact.point;
         var projectileDir = (collision.gameObject.transform.position - contactPos).normalized;
-        projectileDir *= (KnockBack * 10000);
-        collision.gameObject.GetComponent<Rigidbody>().AddForce(projectileDir,ForceMode.Force);
+        projectileDir *= KnockBack * 10000;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(projectileDir, ForceMode.Force);
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("CanPierce"))
-        {
             sphereSolid.enabled = false;
-        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("CanPierce"))
-        {
             sphereSolid.enabled = true;
-        }
     }
 }
